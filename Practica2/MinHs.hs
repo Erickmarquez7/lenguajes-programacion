b0ec1827cfd736b814e6e344e493e76208f30935
Erick B <erick07@ciencias.unam.mx>
2022-05-13 18:32:26 -0500

    avances

--- Practica2/MinHs.hs
+++ Practica2/MinHs.hs
@@ -1,5 +1,8 @@
 module Practica2.MinHs where
 
+import Data.Ix
+import Data.List
+
 -----------------------------------------------------------
 --      Lenguajes de Programación y sus Paradigmas       --  
 -----------------------------------------------------------            
@@ -16,6 +19,7 @@ module Practica2.MinHs where
 --------------------------------------
 
 -- 1. Crear un nuevo tipo de dato el cual representa la sintaxis abstracta para el lenguaje MinHs:
+-- Para las variables
 type Identifier = String
 
 data Expr = V Identifier | I Int | B Bool
@@ -27,7 +31,7 @@ data Expr = V Identifier | I Int | B Bool
             | Lt Expr Expr | Gt Expr Expr | Eq Expr Expr
             | If Expr Expr Expr
             | Let Identifier Expr Expr
-            | App Expr Expr
+            | App Expr Expr deriving (Eq, Show)
 
 
 ----------------------------------------
@@ -36,6 +40,7 @@ data Expr = V Identifier | I Int | B Bool
 
 -- Para las reglas de tipados:
 type TIdentifier = Int
+
 data Type = T TIdentifier
             | Integer | Boolean
             | Arrow Type Type deriving (Eq)
@@ -73,21 +78,41 @@ ables de tipo, obtiene una variable de tipo fresca, es decir, que no aparece en 
 Esta funcion es importante que se realice utilizando lo discutido durante el labo-
 ratorio sobre el problema de MinFree.-}
 
---aquí me quedé u:
+--Primero definimos las siguientes funciones auxiliare
+
+type Nat = Int
+
+minFree :: [Nat] -> Nat
+minFree xs = minFrom 0 (length xs,xs)
+
+minFrom :: Nat -> (Int,[Nat]) -> Nat
+minFrom a (n,xs)
+ | n == 0     = a
+ | m == b-a   = minFrom b (n - m, vs)
+ | otherwise  = minFrom a (m, us)
+ where (us, vs) = partition (< b) xs
+       b = a + 1 + div n 2
+       m = length us
+
 
 fresh :: [Type] -> Type
-fresh = error "D:"
+fresh [] = []
+fresh x = T (minFree ((tvars (head x)) `union` (fresh (tail x))))
 
 
+-- aplicaT s (T n xs) = T n [aplicaT s x | x <- xs]
+-- union xs ys
+-- xs `union` ys
 
-
+m1 :: [Type]
+m1 = [T 1,T 2,T 3,T 5]
+-- fresh m1
 
 --Ejemplos:
 t3 :: [Type]
 t3 =  [T 0,T 1,T 2,T 3] --main > fresh [T0 , T1 , T2 , T3] ⇒ T4
 t4 :: [Type]
 t4 =  [T 0,T 1,T 3,T 4] --main > fresh [T0 , T1 , T3 , T4] ⇒ T2
-
 
 {- 3. (1 pt) Implementar la función rest :: ( [ Type ] , Expr ) → ( [ Type ] , Ctxt , Type
 , Constraint ) la cual dada una expresion, infiere su tipo implementando las reglas descritas
@@ -103,7 +128,6 @@ Integer ) , (T1 , Integer ) ] )
 rest :: ([Type],Expr) -> ([Type],Ctxt,Type,Constraint)
 rest = error "D:"
 
-
 ---------- Algoritmo de Unificación -----------
 
 -- Definimos como una lista de duplas la substitucion en tipos.
@@ -117,9 +141,12 @@ main > subst (T1 → (T2 → T1)) [(3 , T4) , (5 , T6)] ⇒ T1 → (T2 → T1)
 main > subst (T1 → (T2 → T1)) [(1 , T2) , (2 , T3)] ⇒ T2 → (T3 → T2 )
 -}
 subst :: Type -> Substitution -> Type
-subst = error "D:"
-
-
+--subst = error "D:"
+subst (T t) s = case s of 
+                [] -> T t
+                ((x, y): sub) -> if x == t then y else subst (T t) sub
+subst (Arrow t1 t2) s = Arrow (subst t1 s) (subst t2 s)
+subst t s = t
 
 {- 2. (1 pt) Implementar la función comp :: Substitution → Substitution → Substitution
 la cual realiza la composicion de dos sustituciones.
@@ -127,15 +154,17 @@ Ejemplos:
 main > comp [(1 , T2 → T3) , (4 , T5)] [( 2 , T6)] ⇒ [(1 , T6 → T3) , (4 , T5), (2 , T6)]
  -}
 comp :: Substitution -> Substitution -> Substitution
-comp = error "D:"
-
+comp s1 s2 = [(fst t', subst (snd t') s2) | t' <- s1] `union` 
+             [(x,t) | (x,t) <- s2,  x `notElem` [y | (y, t) <- s1]] -- no c pk no jala con snd 
 
 {- 3. (1 pt) Implementar la función unif :: Constraint → Substitution la cual obtiene el unifi-
 cador mas general (μ). Pueden consultar la implementacion realizada durante el laboratorio.
 -}
+-- type Substitution = [(TIdentifier,Type)]
+-- type Constraint = [(Type, Type)]
 unif :: Constraint -> Substitution
-unif = error "D:"
-
+unif [] = []
+ 
 
 ------------- Inferencia de tipos -------------
 {- 1. (1 pt) Implementar la función infer :: Expr > ( Ctxt , Type ) la cual dada una expre-
