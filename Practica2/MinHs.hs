1b7e0af9c44d6046737fec7d8e25be632bab82d2
cesariovb <102694311+cesariovb@users.noreply.github.com>
2022-05-16 00:48:48 -0500

    ya está toda la sección 2 (primeras 3 preguntas) :)

--- Practica2/MinHs.hs
+++ Practica2/MinHs.hs
@@ -67,11 +67,11 @@ tvars (Arrow t s) = myNub (tvars t ++ tvars s)
 
 --Ejemplo: tvars t1 = [1,2], donde t1 se define como:
 t1 :: Type
-t1 = Arrow (T 1) (Arrow (T 2)  (T 1))
+t1 = Arrow (T 1) (Arrow (T 2)  (T 1))   
 
 --Ejemplo: tvars t2 = [], donde t1 se define como:
 t2 :: Type
-t2 = Arrow Integer (Arrow Boolean Integer)
+t2 = Arrow Integer (Arrow Boolean Integer) 
 
 {- 2. (1 pt) Implementar la función fresh :: [Type] → Type la cual dado un conjunto de vari-
 ables de tipo, obtiene una variable de tipo fresca, es decir, que no aparece en este conjunto.
@@ -117,7 +117,7 @@ l2 = [T 0, T 1,T 2,T 3,T 5, Arrow (T 4) (Arrow (T 6)  (T 4))]
 
 --Ejemplo: fresh l3 = T 4, donde l3 se define como:
 l3 :: [Type]
-l3 =  [T 0,T 1,T 2,T 3]
+l3 =  [T 0,T 1,T 2,T 3] 
 
 --Ejemplo: fresh l4 = T 2, donde l4 se define como:
 l4 :: [Type]
@@ -131,139 +131,155 @@ Devolviendo el contexto y el conjunto de restricciones donde es valido. Utiliza 
 {-Primero definimos las funciónes auxiliares que proyectan los diferentes valores del vector
 de cuatro entradas que toma el codominio de la función rest:-}
 p1 :: ([Type],Ctxt,Type,Constraint) -> [Type]
-p1 (a,b,c,d) = a
+p1 (a,b,c,d) = a 
 
 p2 :: ([Type],Ctxt,Type,Constraint) -> Ctxt
-p2 (a,b,c,d) = b
+p2 (a,b,c,d) = b 
 
 p3 :: ([Type],Ctxt,Type,Constraint) -> Type
-p3 (a,b,c,d) = c
+p3 (a,b,c,d) = c 
 
 p4 :: ([Type],Ctxt,Type,Constraint) -> Constraint
 p4 (a,b,c,d) = d
 
-{-También definimos las función auxiliares tvarsR y tvarsC que recibe un conjunto de resticciones (o contextos) 
-y devuelven la lista de los enteros asociados a las variables de tipos que contiene cada restricción (o contexto). 
-Ambas funciones serán de utilidad al definir la función rest para operadores binarios y terciaros, así como para 
-experiones let y aplicaciones-}
+{-También definimos la función auxiliar restrS que modela el comportamiento del conjunto S definido en las reglas  de tipado
+para operadores  binarios, terciaros, así como para  experiones let y aplicaciones. Es decir, restrS recibe dos contextos y si
+una variable aparece en ambos contextos entonces la función añade la restricción correspondiente de tipos-} 
 
-tvarsR :: Constraint -> [TIdentifier]
-tvarsR [] = []
-tvarsR x = tvars (fst (head x)) `union` tvars (snd (head x)) `union` tvarsR (tail x)
+restrS :: Ctxt -> Ctxt -> Constraint
+restrS c1 c2 = [(s1,s2) | (a,s1) <- c1, (b,s2) <- c2, a == b] 
 
---Ejemplo tvarsR r1 = [0,1,2,3,5,4] donde r1 se define como:
-r1 :: Constraint
-r1 = [(T 0, T 1),(T 1, T 2),(T 3, Arrow (T 1) (T 5)),(T 4, Integer)]
-
-tvarsC :: Ctxt -> [TIdentifier]
-tvarsC [] = []
-tvarsC x = tvars (snd (head x)) `union` tvarsC (tail x)
-
---Ejemplo tvarsC r2 = [1,2,5] donde r1 se define como:
+--Ejemplo: restrS r1 r2 = [(T 0,Arrow (T 3) (T 4))], y restrS r1 r3 = [] donde r1, r2 y r3 se definen como:
+r1 :: Ctxt
+r1 = [("x",T 0),("y", T 1)] 
 r2 :: Ctxt
-r2 = [("x", T 1),("y", T 2),("z", Arrow (T 1) (T 5))]
+r2 = [("x",Arrow (T 3) (T 4)),("w", T 2)]
+r3 :: Ctxt
+r3 = [("z",T 10),("w", T 22)]
 
---Teniendo en cuenta las funciones auxiliares p1, p2, p3, p4, tvarsR y tvarsC podemos definir rest de la siguiente forma:
+{-Teniendo en cuenta las funciones auxiliares p1, p2, p3, p4, tvarsR, tvarsC y restrS podemos definir 
+la función rest de la siguiente forma:-}
 rest :: ([Type],Expr) -> ([Type],Ctxt,Type,Constraint)
 --Tipado de variables
-rest (l, V x) = (l ++ [fresh l], [(x,fresh l)],fresh l,[])
+rest (l, V x) = (l ++ [fresh l],[(x,fresh l)],fresh l,[])
 -- Tipado de constantes
 rest (l, I n) = (l, [],Integer,[])
 rest (l, B b) = (l, [],Boolean,[])
 --Tipado de funciones
-rest (l, Fn x e) = (l ++ p1 (rest (l,V x)),
-                   p2 (rest (l,e)) `union` p2 (rest (l,V x)),
-                   Arrow (p3 (rest (l,V x))) (p3 (rest (l,e))),
+rest (l, Fn x e) = (l `union` p1 (rest (l, V x)) `union` p1 (rest (l,e)),
+                   p2 (rest (l,e)) \\ [(x, p3 (rest (l, V x)))],
+                   Arrow (p3 (rest (l, V x))) (p3 (rest (l,e))),
                    p4 (rest (l,e)))
 --Tipado de operadores unarios                   
-rest (l, Succ e) = (l,
+rest (l, Succ e) = (l `union` p1 (rest (l,e)),
                    p2 (rest (l,e)),
                    Integer,
                    p4 (rest (l,e)) ++ [(p3 (rest (l,e)),Integer)])
-rest (l, Pred e) = (l,
+rest (l, Pred e) = (l  `union` p1 (rest (l,e)),
                    p2 (rest (l,e)),
                    Integer,
                    p4 (rest (l,e)) ++ [(p3 (rest (l,e)),Integer)])
-rest (l, Not e) = (l,
+rest (l, Not e) = (l  `union` p1 (rest (l,e)),
                    p2 (rest (l,e)),
                    Boolean,
                    p4 (rest (l,e)) ++ [(p3 (rest (l,e)),Boolean)])
-rest (l, Iszero e) = (l,
+rest (l, Iszero e) = (l  `union` p1 (rest (l,e)),
                    p2 (rest (l,e)),
                    Boolean,
-                   p4 (rest (l,e)) ++ [(p3 (rest (l,e)),Boolean)])
+                   p4 (rest (l,e)) ++ [(p3 (rest (l,e)),Boolean)])                   
 --Tipado de operadores binarios y terciarios:
-rest (l, Add e1 e2) = if null (tvarsR (p4 (rest (l,e1))) `union` tvarsC (p2 (rest (l,e1))) `union` tvars (p3 (rest (l,e1)))
-                         `intersect`
-                         tvarsR (p4 (rest (l,e2))) `union` tvarsC (p2 (rest (l,e2))) `union` tvars (p3 (rest (l,e2))))
-                              then (l,
-                                    p2 (rest (l,e1)) `union` p2 (rest (l,e2)),
-                                    Integer,
-                                    p4 (rest (l,e1)) ++ p4 (rest (l,e2)) ++ [(p3 (rest (l,e1)),Integer),(p3 (rest (l,e2)),Integer)]) --OJO falta modelar como añadir el conjunto S :w
-                                          else error "los conjuntos de las variables libres asociadas a las expresiones e1 y e2 no son disjuntos"
 
---Mul Expr Expr
-
---And Expr Expr  
-
---Or Expr Expr
-
---Lt Expr Expr 
-
---Gt Expr Expr 
-
---Eq Expr Expr
-
---If Expr Expr Expr
-
+--Suma
+rest (l, Add e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        Integer, 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Integer),(p3 (rest (l++p1 (rest (l,e1)),e2)),Integer)]) --los tipos de e1 y e2 deben ser enteros
+--Producto
+rest (l, Mul e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        Integer, 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Integer),(p3 (rest (l++p1 (rest (l,e1)),e2)),Integer)]) --los tipos de e1 y e2 deben ser enteros
+--Conjunción
+rest (l, Or e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        Boolean, 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Boolean),(p3 (rest (l++p1 (rest (l,e1)),e2)),Boolean)]) --los tipos de e1 y e2 deben ser booleanos
+--Disyunción
+rest (l, And e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        Boolean, 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Boolean),(p3 (rest (l++p1 (rest (l,e1)),e2)),Boolean)]) --los tipos de e1 y e2 deben ser booleanos
+--Menor que
+rest (l, Lt e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        Boolean, 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Integer),(p3 (rest (l++p1 (rest (l,e1)),e2)),Integer)]) --los tipos de e1 y e2 deben ser enteros
+--Mayor que
+rest (l, Gt e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        Boolean, 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Integer),(p3 (rest (l++p1 (rest (l,e1)),e2)),Integer)]) --los tipos de e1 y e2 deben ser enteros
+--Igualdad numérica
+rest (l, Eq e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        Boolean, 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Integer),(p3 (rest (l++p1 (rest (l,e1)),e2)),Integer)]) --los tipos de e1 y e2 deben ser enteros
+--Condicional If   
+rest (l, If e e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p3 (rest (l,e1)), 
+                        [(p3 (rest (l,e)),Boolean)] --restricciones del tipado de e
+                        ++ p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l++p1 (rest (l,e1)),e2)),p3 (rest (l,e1)))]) --los tipos de e1 y e2 deben ser iguales
 --Tipado de aplicaciones
-
---App Expr Expr 
-
+rest (l, App e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        fresh (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2))), 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),Arrow (p3 (rest (l,e2))) (fresh (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)))))]) {-el tipo de la 
+                        expresión e1 debe ser igual al tipo Arrow t2 Z, donde t2 es el tipo de la expresión e2 y Z es la variable fresca a la que pertenece la aplicación-}
 --Tipado de expresión Let
+rest (l, Let x e1 e2) = (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p2 (rest (l,e1)) ++ p2 (rest (l++p1 (rest (l,e1)),e2)), 
+                        p3 (rest (l,e2)), 
+                        p4 (rest (l,e1)) --restricciones del tipado de e1
+                        ++ p4 (rest (l,e2)) --restricciones del tipado de e2
+                        ++ restrS (p2 (rest (l,e1))) (p2 (rest (l++p1 (rest (l,e1)),e2))) --restricciones del conjunto S definido en el pdf de la práctica
+                        ++ [(p3 (rest (l,e1)),p3 (rest (l `union` p1 (rest (l,e1)) `union` p1 (rest (l++p1 (rest (l,e1)),e2)),V x)))]) {-el tipo de la expresión 
+                        e1 debe ser igual al tipo de la variable xArrow t2 Z, donde t2 es el tipo de la expresión e2 y Z es la variable fresca a la que pertenece la aplicación-}
 
---Let Identifier Expr Expr
-
-rest x = error "D:"
-
-
---No hacer caso jeje
-----------------------------------------------------------------------------------------------------------------------------------------------------                                                                                                    
-
---p1 :: ([Type],Ctxt,Type,Constraint) -> [Type]
-
-{-type TIdentifier = Int
-
-data Type = T TIdentifier
-            | Integer | Boolean
-            | Arrow Type Type deriving (Show, Eq)
-
--- Contexto al igual que en al anterior practica, para verificacion de tipos:
-type Ctxt = [(Identifier, Type)]
-
--- Conjunto de restricciones
-type Constraint = [(Type, Type)]-}
-
-
-{-data Expr = V Identifier | I Int | B Bool
-            | Fn Identifier Expr
-            | Succ Expr | Pred Expr
-            | Add Expr Expr | Mul Expr Expr
-            | Not Expr | Iszero Expr
-            | And Expr Expr | Or Expr Expr
-            | Lt Expr Expr | Gt Expr Expr | Eq Expr Expr
-            | If Expr Expr Expr
-            | Let Identifier Expr Expr
-            | App Expr Expr deriving (Eq, Show)-}
-
-
---Ejemplos:
-{-main > rest ( [] , Fn ”x” (V ”x” ) ) ⇒
-( [ T0 ] , [ ] , T0 > T0 , [ ] )
-main > rest ( [] , Add (V ”x” ) (V ”x” ) ) ⇒
-( [ T0 , T1 ] , [ ( ”x” , T0 ) , ( ”x” , T1 ) ] , Integer , [ ( T0 , T1 ) , (T0 ,
-Integer ) , (T1 , Integer ) ] )-}
-----------------------------------------------------------------------------------------------------------------------------------------------------
+--Ejemplo: rest ([], exp1) = ([T 0],[],Arrow T0 T0,[]), donde exp1 se define como:
+exp1 :: Expr
+exp1 = Fn "x" (V "x" )
+--Ejemplo: rest ([], exp2) = , donde exp2 se define como:
+exp2 :: Expr
+exp2 = Add (V "x") (V "x")
+--Ejemplo: rest ([T 0, T 1, T 2], exp1) = ([T 0,T 1,T 2,T 3],[("x",T 3)],Arrow (T 3) (T 3),[]) 
 
 -------------------------------------
 --    Algoritmo de Unificación U   --
@@ -271,7 +287,6 @@ Integer ) , (T1 , Integer ) ] )-}
 
 -- Definimos como una lista de duplas la substitucion en tipos.
 type Substitution = [(TIdentifier,Type)]
-
 
 {- 1. (1 pt) Implementar la función subst :: Type → Substitution → Type la cual aplica la
 sustitucion a un tipo dado.
@@ -285,7 +300,6 @@ subst (T t) [] = T t
 subst (T t) ((ti,T t'):xs) = if t == ti then T t' else subst (T t) xs
 subst (Arrow t1 t2) s = Arrow (subst t1 s) (subst t2 s)
 subst t s = t
-
 
 {- 2. (1 pt) Implementar la función comp :: Substitution → Substitution → Substitution
 la cual realiza la composicion de dos sustituciones.
@@ -333,10 +347,10 @@ unifaux' t s
  | t == s = [[]] 
  | otherwise = error "No se puede hacer la unificación :("
 
+------------------------------------------
+--         Inferencia de tipos          --
+------------------------------------------
 
-
-
-------------- Inferencia de tipos -------------
 {- 1. (1 pt) Implementar la función infer :: Expr > ( Ctxt , Type ) la cual dada una expre-
 sion, infiere su tipo devolviendo el contexto donde es valido.
 Ejemplos:
@@ -364,7 +378,6 @@ infer' (ts, V x) =
        let n = fresh v
            tl = v `union` [n]
            in (tl, [(i,n)], n, []) -- i debe ser string
-
 infer' (v, B b) = 
        let n = fresh v
            tl = v `union` [n]
@@ -376,16 +389,9 @@ infer' (v, Succ e) = let (v, g, t, r) = infer' (v, e)
                               in
                                 (nv', g, [z], r)
 
-
 infer :: Expr -> (Ctxt, Type)
 infer = error "D:"
-
-
-
-
-
 
 -----------------------------------
 --stack ghci src/Practica2.Minhs.hs
 -----------------------------------
-
