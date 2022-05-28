e895a9dc409c0ee3fa1f62884df8b748b0aa9ad2
cesariovb <102694311+cesariovb@users.noreply.github.com>
2022-05-27 23:36:50 -0500

    Update MiniC.hs
    Ya está la primera parte de memoria

--- Practica3/MiniC.hs
+++ Practica3/MiniC.hs
@@ -2,6 +2,7 @@
 
 module Practica3.MiniC where
 import Data.List
+import Data.Maybe
 -----------------------------------------------------------
 --      Lenguajes de Programación y sus Paradigmas       --  
 -----------------------------------------------------------            
@@ -43,37 +44,106 @@ type Address = Int
 type Value = Expr
 type Cell = (Address, Value)
 type Memory = [Cell]
---            [(Adre, Val), (Adre, Val), (Adre, Val),...]
+--[(Adre, Val), (Adre, Val), (Adre, Val),...]
 
+-------------------
+--    Memoria    --
+-------------------
 
--- Dada una nueva de memoria, genera una nueva direccion de memoria que no esté contenida en esta
-newAdress :: Memory -> Expr
-newAdress [] = L 0
--- faltan casos xd
+{-En toda esta sección de la práctica se usará la función auxiliar revMemo la cual verifica si una memoria está corrupta
+ (es decir, revMemo verifica si existen dos celdas cuya primera entrada es la misma):-}
+revMemo :: Memory -> Memory
+revMemo [] = []
+revMemo m = if fst (head m) `elem` map fst (tail m) then error "Corrupted Memory" else head m : revMemo (tail m)
 
+--Implementa las sigientes funciones y las definiciones anteriores en un módulo llamado Memory:
 
+--1. newAdress. Dada una nueva de memoria, genera una nueva direccion de memoria que no esté contenida en esta
 
+{-Primero recordemos la función auxiliar minFree utilizada en la Práctica 2, esta función recibe una lista de números naturales
+ y regresa el mínimo número natural de la lista que no está en ella, requiere de la función auxiliar minFrom y 
+ la lista que reciba no debe tener elementos repetidos pues se cicla.-}
+type Nat = Int
 
+minFree :: [Nat] -> Nat
+minFree xs = minFrom 0 (length xs,xs)
 
+minFrom :: Nat -> (Int,[Nat]) -> Nat
+minFrom a (n,xs)
+ | n == 0     = a
+ | m == b-a   = minFrom b (n - m, vs)
+ | otherwise  = minFrom a (m, us)
+ where (us, vs) = partition (< b) xs
+       b = a + 1 + div n 2
+       m = length us
 
--- Dada una dirección de memoria, devuelve el valor contenido en la celda con tal
+--Así podemos definir newAdress como:
+
+newAddress :: Memory -> Expr
+newAddress [] = L 0
+newAddress m = L (minFree (map fst (revMemo m)))
+
+{-Ejemplos:
+
+                                     newAddress [] = L0
+                  newAddress [(0,B False),(2,I 9)] = L1
+           newAddress [(0,I 21),(1,Void),(2,I 12)] = L3
+newAddress [(0,I 21),(1,Void),(2,I 12),(1,B True)] = Error: Corrupted Memory
+-}
+
+--2. acces. Dada una dirección de memoria, devuelve el valor contenido en la celda con tal
 -- dirección, en caso de no encontrarla debe devolver Nothing.
 access :: Address -> Memory -> Maybe Value
 access _ [] = Nothing
--- faltan xd
+access i m = if null ([x | x <- revMemo m, fst x == i]) 
+                then Nothing 
+                    else Just (snd (head [x | x <- revMemo m, fst x == i]))
 
+{-Ejemplos:
 
-
+                                      access 3 [] = Nothing
+                 access 1 [(0,B False ),(2, I 9)] = Nothing
+            access 2 [(0,I 21),(2,I 12),(1,Void)] = Just (I 12)
+access 2 [(0,I 21),(0,B False),(3,Void),(2,I 12)] = Exception : Corrupted memory.
+-}
 
 -- Dada una celda de memoria, actualiza el valor de esta misma en la memoria. En
 -- caso de no existir debe devolver Nothing.
 update :: Cell -> Memory -> Maybe Memory
-update _ [] = Nothing
--- falta xd
+update (i,valor) [] = case valor of B False -> Nothing
+                                    B True  -> Nothing
+                                    I n     -> Nothing
+                                    Fn a b  -> Nothing
+                                    valor   -> error "Memory can only store values"
+update (i,valor) m  = case valor of B False -> Just ([x | x <- revMemo m, i /= fst x] ++ [(i,B False)])
+                                    B True  -> Just ([x | x <- revMemo m, i /= fst x] ++ [(i,B True)])
+                                    I n     -> Just ([x | x <- revMemo m, i /= fst x] ++ [(i,I n)])
+                                    Fn a b  -> Just ([x | x <- revMemo m, i /= fst x] ++ [(i,Fn a b)])      
+                                    valor   -> error "Memory can only store values"                           
 
+{-Ejemplos:
+                                  update (3,B True) [] = Nothing
+        update (0,Succ (V "x")) [(0,B False),(2, I 9)] = Exception : Memory can only store values
+update (0,Fn "x" (V "x")) [(0,I 21),(1,Void),(2,I 12)] = [(0,Fn "x" (V "x")),(1,Void),(2,I 12)]
+       update (2, I 14) [(0,I 21),(2, Void),(2, I 12)] = Exception : Corrupted memory
+       update (2, I 14) [(0,I 13),(1,B True),(2,I 25)] = Just [(0,I 13),(1,B True),(2,I 14)]
+-}
 
+--------------------------------     -----------------------
+--    Ejecución Secuencial    --  y  --    Ciclo While    --
+--------------------------------     -----------------------
 
+{-Se añadieron los constructores: 
 
+                                  | Void
+                                  | Seq Expr Expr
+                                  | While Expr Expr
+al tipo de dato Expr
+-}
+
+--------------------------------------------
+--    Reimplementación de la semántica    --
+--------------------------------------------
 
 -- Lo mismo que en la practica 1 solo que extendida
 frVars :: Expr -> [Identifier]
@@ -149,7 +219,6 @@ subst (Seq a b) s = Seq (subst a s) (subst b s)
 subst (While a b) s = While (subst a s) (subst b s)
 subst (App a b) s = App (subst a s) (subst b s)
 
-
 -- Re implementa esta función para que dada una memoria y una expresión, devuelva
 --la reducción a un paso, es decir, eval1 (m, e) = (m’, e’) si y sólo si <m, e> → <m’,e’>
 {-data Expr = V Identifier | I Int | B Bool
@@ -192,3 +261,9 @@ evals = error "D:"
 --un mensaje de error particular del operador que lo causó.
 evale :: Expr -> Expr
 evale = error "D:"
+
+
+
+-----------------------------------
+--stack ghci src/Practica3.MiniC.hs
+-----------------------------------
