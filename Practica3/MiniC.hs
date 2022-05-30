7a864f5550deec12fcf8ea2c40ac032cc5cff77b
cesariovb <102694311+cesariovb@users.noreply.github.com>
2022-05-29 23:21:04 -0500

    solo-falta-la-última

--- Practica3/MiniC.hs
+++ Practica3/MiniC.hs
@@ -1,9 +1,4 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use second" #-}
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
-{-# HLINT ignore "Use bimap" #-}
-
 module Practica3.MiniC where
 import Data.List
 import Data.Maybe
@@ -332,31 +327,32 @@ eval1 (m,If e e1 e2) = (fst (eval1 (m,e)),If (snd (eval1 (m,e))) e1 e2)
 -- Let String Expr Expr
 eval1 (m,Let x (B b) e) = (m,subst e (x, B b))
 eval1 (m,Let x (I n) e) = (m,subst e (x, I n))
+eval1 (m,Let x (Fn y e) e1) = (m,subst e1 (x, Fn y e))
 eval1 (m,Let x e1  e2) = (fst (eval1 (m,e1)),Let x (snd (eval1 (m,e1))) e2)
 -- L Int
 eval1 (m, L i) = (m,L i)
 -- Alloc Expr
 -- Una expresión de la forma ref v (Alloc v) donde v es valor, devuelve como valor la dirección simbólica de la celda que alojará a v, la cual se agrega a la memoria en uso:
-eval1 (m, Alloc e) = case e of I n    -> (m ++ [(loC (newAddress m), I n)], newAddress m)
-                               B b    -> (m ++ [(loC (newAddress m), B b)], newAddress m)
-                               Fn x e -> (m ++ [(loC (newAddress m), Fn x e)], newAddress m)
-                               e      -> (fst (eval1 (m,e)), Alloc (snd (eval1 (m,e)))) --Para evaluar una expresión de la forma ref v (Alloc v) primero hay que reducir e hasta que sea un valor.
+eval1 (m,Alloc (I n))    = (m ++ [(loC (newAddress m), I n)], newAddress m)
+eval1 (m,Alloc (B b))    = (m ++ [(loC (newAddress m), B b)], newAddress m)
+eval1 (m,Alloc (Fn x e)) = (m ++ [(loC (newAddress m), Fn x e)], newAddress m)
+eval1 (m,Alloc e)      = (fst (eval1 (m,e)), Alloc (snd (eval1 (m,e)))) --Para evaluar una expresión de la forma ref v (Alloc v) primero hay que reducir e hasta que sea un valor.
 -- Dref Expr
 -- Una recuperación ! (Dref L n) se evalua al valor almacenado en la celda con dirección de memoria "L n" el cual está dado por μ(L n).
-eval1 (m, Dref e) = case e of L n    -> (m, fromJust (access n m))
-                              I n    -> (m , I n) --Dred (!) solo se aplica a lugares de celda, por lo que Dref de un valor o es el mismo valor o es un error (por ejemplo !2 = 2 o !2 = error "el operador recuperación solo admite lugares de celda", preguntaré por telegram)
-                              B b    -> (m , B b)
-                              Fn x e -> (m , Fn x e)     
-                              e      -> (fst (eval1 (m,e)), Dref (snd (eval1 (m,e))))
+eval1 (m, Dref (L n))    = (m, fromJust (access n m))
+eval1 (m, Dref (I n))    = (m , I n) --Dred (!) solo se aplica a lugares de celda, por lo que Dref de un valor o es el mismo valor o es un error (por ejemplo !2 = 2 o !2 = error "el operador recuperación solo admite lugares de celda", preguntaré por telegram)
+eval1 (m, Dref (B b))    = (m , B b)
+eval1 (m, Dref (Fn x e)) = (m , Fn x e)
+eval1 (m, Dref e)        = (fst (eval1 (m,e)), Dref (snd (eval1 (m,e))))
 -- Assig Expr Expr
-eval1 (m, Assig (L n) e2) = case e2 of (L n)     -> error "Se le dará un lugar de memoria a un lugar de memoria, algo anda mal D:"
-                                       (I i)     -> (fromJust (update (n,I i) m),Void)   ----v----
-                                       (B True)  -> (fromJust (update (n,B True) m),Void) ---v---
-                                       (B False) -> (fromJust (update (n,B False) m),Void) --v--
-                                       (Fn x e)  -> (fromJust (update (n,Fn x e) m),Void) {-Una asignación (L n) := v (v es valor) causa un efecto en la memoria y devuelve un valor irrelevante () (Void). 
+eval1 (m, Assig (L n) (L i))     = error "Se le dará un lugar de memoria a un lugar de memoria, algo anda mal D:"
+eval1 (m, Assig (L n) (I i))     = (fromJust (update (n,I i) m),Void)   ----v----
+eval1 (m, Assig (L n) (B True))  = (fromJust (update (n,B True) m),Void) ---v---
+eval1 (m, Assig (L n) (B False)) = (fromJust (update (n,B False) m),Void) --v--
+eval1 (m, Assig (L n) (Fn x e))  = (fromJust (update (n,Fn x e) m),Void) {-Una asignación (L n) := v (v es valor) causa un efecto en la memoria y devuelve un valor irrelevante () (Void). 
                                                                                                El efecto consiste en eliminar el valor almacenado en la celda cuya dirección es (L n),
                                                                                                guardando en su lugar el valor dado v.-}
-                                       e2        -> (fst (eval1 (m,e2)), Assig (L n) (snd (eval1 (m,e2)))) --Para evaluar una asignación (L n) := e2 primero es necesario reducir e2
+eval1 (m, Assig (L n) e2)        = (fst (eval1 (m,e2)), Assig (L n) (snd (eval1 (m,e2)))) --Para evaluar una asignación (L n) := e2 primero es necesario reducir e2
 eval1 (m, Assig e1 e2) = (fst (eval1 (m,e1)), Assig (snd (eval1 (m,e1))) e2) --Para evaluar una asignación e1 := e2 primero es necesario reducir e1.
 -- Void
 eval1 (m,Void) = (m,Void)
@@ -366,16 +362,15 @@ eval1 (m,Seq e1 e2) = (fst (eval1 (m,e1)), Seq (snd (eval1 (m,e1))) e2)
 --While Expr Expr
 eval1 (m,While e1 e2) = (m,If e1 (Seq e2 (While e1 e2)) Void)
 --App Expr Expr 
-eval1 (m,App (Fn x e) valor) = case valor of I i     -> (m, subst (Fn x e) (x, I i))
-                                             B True  -> (m, subst (Fn x e) (x, B True))
-                                             B False -> (m, subst (Fn x e) (x, B False))
-                                             Fn y g  -> (m, subst (Fn x e) (x, Fn y g))
-eval1 (m,App e1 e2) = case e1 of I i     -> (fst (eval1 (m,e2)), App (I i) (snd (eval1 (m,e2))))
-                                 B True  -> (fst (eval1 (m,e2)), App (B True) (snd (eval1 (m,e2))))
-                                 B False -> (fst (eval1 (m,e2)), App (B False) (snd (eval1 (m,e2))))
-                                 Fn x e  -> (fst (eval1 (m,e2)), App (Fn x e) (snd (eval1 (m,e2))))
-                                 e1      -> (fst (eval1 (m,e1)), App (snd (eval1 (m,e1))) e2) --Las reglas para evaluar una expresión App se encuentran en las Notas de Clase 6 p.2
-
+eval1 (m,App (Fn x e) (I i)) = (m, subst (Fn x e) (x, I i))
+eval1 (m,App (Fn x e) (B True)) = (m, subst (Fn x e) (x, B True))
+eval1 (m,App (Fn x e) (B False)) = (m, subst (Fn x e) (x, B False))
+eval1 (m,App (Fn x e) (Fn y g))  = (m, subst (Fn x e) (x, Fn y g))
+eval1 (m,App (I i) e2)     = (fst (eval1 (m,e2)), App (I i) (snd (eval1 (m,e2))))
+eval1 (m,App (B True) e2)  = (fst (eval1 (m,e2)), App (B True) (snd (eval1 (m,e2))))
+eval1 (m,App (B False) e2) = (fst (eval1 (m,e2)), App (B False) (snd (eval1 (m,e2))))
+eval1 (m,App (Fn x e) e2)  = (fst (eval1 (m,e2)), App (Fn x e) (snd (eval1 (m,e2))))
+eval1 (m,App e1 e2)        = (fst (eval1 (m,e1)), App (snd (eval1 (m,e1))) e2) --Las reglas para evaluar una expresión App se encuentran en las Notas de Clase 6 p.2
 {-Ejemplos:
 
                    eval1 ([(0,B False)],(Add (I 1) (I 2))) = ([(0,B False)],I 3)
@@ -389,79 +384,54 @@ eval1 ([(0,B False)], (Let "x" (I 1) (Add (V "x") (I 2)))) = ([(0, B False)], Ad
 -- <m’, e’> y e’ esta bloqueado.
 evals :: (Memory, Expr) -> (Memory, Expr)
 -- Valores
-evals (m,valor) = case valor of V x    -> eval1 (m,V x)
-                                I n    -> eval1 (m,I n)
-                                B b    -> eval1 (m,B b)
-                                Fn x e -> evals (m,Fn x e)
+evals (m,V x)  = eval1 (m,V x)
+evals (m,I n)    = eval1 (m,I n)
+evals (m,B b)    = eval1 (m,B b)
+evals (m,Fn x e) = evals (m,Fn x e)
 -- Operadore unarios
-evals (m,opUnario) = case opUnario of Succ e   -> eval1 (fst (evals (m,e)), Succ (snd (evals (m,e))))
-                                      Pred e   -> eval1 (fst (evals (m,e)), Pred (snd (evals (m,e))))
-                                      Not e    -> eval1 (fst (evals (m,e)), Not (snd (evals (m,e))))
-                                      Iszero e -> eval1 (fst (evals (m,e)), Iszero (snd (evals (m,e))))
+evals (m,Succ e)   = eval1 (fst (evals (m,e)), Succ (snd (evals (m,e))))
+evals (m, Pred e)  = eval1 (fst (evals (m,e)), Pred (snd (evals (m,e))))
+evals (m,Not e)    = eval1 (fst (evals (m,e)), Not (snd (evals (m,e))))
+evals (m,Iszero e) = eval1 (fst (evals (m,e)), Iszero (snd (evals (m,e))))
 -- Operadore binarios
-evals (m,opBinario) = case opBinario of Add e1 e2 -> eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)), Add (snd (evals (m,e1))) (snd (evals (m,e2))))
-                                        Mul e1 e2 -> eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)), Mul (snd (evals (m,e1))) (snd (evals (m,e2))))
-                                        And e1 e2 -> eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)), And (snd (evals (m,e1))) (snd (evals (m,e2))))
-                                        Or e1 e2  -> eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)), Or (snd (evals (m,e1))) (snd (evals (m,e2))))
-                                        Gt e1 e2  -> eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)), Gt (snd (evals (m,e1))) (snd (evals (m,e2))))
-                                        Lt e1 e2  -> eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)), Lt (snd (evals (m,e1))) (snd (evals (m,e2))))
-                                        Eq e1 e2  -> eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)), Eq (snd (evals (m,e1))) (snd (evals (m,e2))))
+evals (m,Add e1 e2) = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), Add (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
+evals (m,Mul e1 e2) = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), Mul (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
+evals (m,And e1 e2) = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), And (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
+evals (m,Or e1 e2)  = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), Or  (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
+evals (m,Gt e1 e2)  = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), Gt  (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
+evals (m,Lt e1 e2)  = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), Lt  (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
+evals (m,Eq e1 e2)  = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), Eq  (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
 -- If Expr Expr Expr
-evals (m,If e e1 e2) = eval1 (fst (evals (m,e)) ++ fst (evals (m,e1)) ++ fst (evals (m,e2)),
-                               If (snd (evals (m,e))) (snd (evals (m,e1))) (snd (evals (m,e2))))
+evals (m,If e e1 e2) = eval1 (fst (evals (m,e)) `union` fst (evals (fst (evals (m,e)),e1)) `union` fst (evals (fst (evals (fst (evals (m,e)),e1)),e2)),
+                                   If (snd (evals (m,e))) (snd (evals (fst (evals (m,e)),e1))) (snd (evals (fst (evals (fst (evals (m,e)),e1)),e2))))
 -- Let String Expr Expr
-evals (m,Let x e1 e2) = eval1 (fst (evals (m,e1)) ++ fst (evals (m,e2)),
-                               Let x (snd (evals (m,e1))) (snd (evals (m,e2))))
+evals (m,Let x (I i) e2) = evals (eval1 (m,Let x (I i) e2))
+evals (m,Let x (B b) e2) = evals (eval1 (m,Let x (B b) e2))
+evals (m,Let x (Fn y e) e2) = evals (eval1 (m,Let x (Fn y e) e2))
+evals (m,Let x e1 e2) = evals (fst (evals (m,e1)), Let x (snd (evals (m,e1))) e2)
 -- L Int
 evals (m,L i) = eval1 (m,L i)
-
---Hasta aquí llegué gg
-
-{-
 -- Alloc Expr
--- Una expresión de la forma ref v (Alloc v) donde v es valor, devuelve como valor la dirección simbólica de la celda que alojará a v, la cual se agrega a la memoria en uso:
-eval1 (m, Alloc e) = case e of I n    -> (m ++ [(loC (newAddress m), I n)], newAddress m)
-                               B b    -> (m ++ [(loC (newAddress m), B b)], newAddress m)
-                               Fn x e -> (m ++ [(loC (newAddress m), Fn x e)], newAddress m)
-                               e      -> (fst (eval1 (m,e)), Alloc (snd (eval1 (m,e)))) --Para evaluar una expresión de la forma ref v (Alloc v) primero hay que reducir e hasta que sea un valor.
+evals (m, Alloc e) = eval1 (fst (evals (m,e)), Alloc (snd (evals (m,e))))
 -- Dref Expr
 -- Una recuperación ! (Dref L n) se evalua al valor almacenado en la celda con dirección de memoria "L n" el cual está dado por μ(L n).
-eval1 (m, Dref e) = case e of L n    -> (m, fromJust (access n m))
-                              I n    -> (m , I n) --Dred (!) solo se aplica a lugares de celda, por lo que Dref de un valor o es el mismo valor o es un error (por ejemplo !2 = 2 o !2 = error "el operador recuperación solo admite lugares de celda", preguntaré por telegram)
-                              B b    -> (m , B b)
-                              Fn x e -> (m , Fn x e)     
-                              e      -> (fst (eval1 (m,e)), Dref (snd (eval1 (m,e))))
+evals (m, Dref e) = eval1 (fst (evals (m,e)), Dref (snd (evals (m,e))))
 -- Assig Expr Expr
-eval1 (m, Assig (L n) e2) = case e2 of (L n)     -> error "Se le dará un lugar de memoria a un lugar de memoria, algo anda mal D:"
-                                       (I i)     -> (fromJust (update (n,I i) m),Void)   ----v----
-                                       (B True)  -> (fromJust (update (n,B True) m),Void) ---v---
-                                       (B False) -> (fromJust (update (n,B False) m),Void) --v--
-                                       (Fn x e)  -> (fromJust (update (n,Fn x e) m),Void) {-Una asignación (L n) := v (v es valor) causa un efecto en la memoria y devuelve un valor irrelevante () (Void). 
-                                                                                               El efecto consiste en eliminar el valor almacenado en la celda cuya dirección es (L n),
-                                                                                               guardando en su lugar el valor dado v.-}
-                                       e2        -> (fst (eval1 (m,e2)), Assig (L n) (snd (eval1 (m,e2)))) --Para evaluar una asignación (L n) := e2 primero es necesario reducir e2
-eval1 (m, Assig e1 e2) = (fst (eval1 (m,e1)), Assig (snd (eval1 (m,e1))) e2) --Para evaluar una asignación e1 := e2 primero es necesario reducir e1.
+evals (m, Assig e1 e2) = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)), Assig (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
 -- Void
-eval1 (m,Void) = (m,Void)
+evals (m,Void) = eval1 (m,Void)
 -- Seq Expr Expr
-eval1 (m,Seq Void e2) = (m, e2)
-eval1 (m,Seq e1 e2) = (fst (eval1 (m,e1)), Seq (snd (eval1 (m,e1))) e2)
+evals (m, Seq e1 e2) = eval1 (fst (evals (m,e1)), Seq (snd (evals (m,e1))) e2)
 --While Expr Expr
-eval1 (m,While e1 e2) = (m,If e1 (Seq e2 (While e1 e2)) Void)
+evals (m, While e1 e2) = (m,If e1 (Seq e2 (While e1 e2)) Void)
 --App Expr Expr 
-eval1 (m,App (Fn x e) valor) = case valor of I i     -> (m, subst (Fn x e) (x, I i))
-                                             B True  -> (m, subst (Fn x e) (x, B True))
-                                             B False -> (m, subst (Fn x e) (x, B False))
-                                             Fn y g  -> (m, subst (Fn x e) (x, Fn y g))
-eval1 (m,App e1 e2) = case e1 of I i     -> (fst (eval1 (m,e2)), App (I i) (snd (eval1 (m,e2))))
-                                 B True  -> (fst (eval1 (m,e2)), App (B True) (snd (eval1 (m,e2))))
-                                 B False -> (fst (eval1 (m,e2)), App (B False) (snd (eval1 (m,e2))))
-                                 Fn x e  -> (fst (eval1 (m,e2)), App (Fn x e) (snd (eval1 (m,e2))))
-                                 e1      -> (fst (eval1 (m,e1)), App (snd (eval1 (m,e1))) e2) --Las reglas para evaluar una expresión App se encuentran en las Notas de Clase 6 p.2
--}
+evals (m, App e1 e2) = eval1 (fst (evals (m,e1)) `union` fst (evals (fst (evals (m,e1)),e2)),App (snd (evals (m,e1))) (snd (evals (fst (evals (m,e1)),e2))))
 
-------------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------------------------------
+{- Ejemplos:
+evals ([],(Let "x" (Add (I 1) (I 2)) (Eq (V "x") (I 0)))) = ([],B False)
+              evals ([],(Add (Mul (I 2) (I 6)) (B True))) = ([],Add (I 12) (B True))
+    evals ([], Assig (Alloc (B False)) (Add (I 1) (I 9))) = ([(0,I 10)],Void)
+-}
 
 --5. evale. Extiende esta función para que dada una expresión, devuelva la evaluación de un
 --programa tal que evale e = e’ syss e → e’ y e’ e un valor. En caso de que e’ no sea un valor deberá mostrar
